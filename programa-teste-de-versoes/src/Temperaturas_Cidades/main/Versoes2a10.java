76a8e7f9c57b2e66072100583db9390e0e82144b
Scarllet Gomes <86412432+scarlletgomes@users.noreply.github.com>
2024-09-18 15:08:08 -0300

    Criação Versões 6 a 10

--- programa-teste-de-versoes/src/Temperaturas_Cidades/main/Versoes2a10.java
+++ programa-teste-de-versoes/src/Temperaturas_Cidades/main/Versoes2a10.java
@@ -6,7 +6,6 @@ public class Versoes2a10 {
 	void iniciarV2(){
 		System.out.println("TEMPERATURAS CIDADES\n");
 		configT.setThreads(2);
-		
 	}
 	
 	void iniciarV3(){
@@ -25,23 +24,28 @@ public class Versoes2a10 {
 	}
 	
 	void iniciarV6(){
-		
+		System.out.println("TEMPERATURAS CIDADES\n");
+		configT.setThreads(32);
 	}
 	
 	void iniciarV7(){
-		
+		System.out.println("TEMPERATURAS CIDADES\n");
+		configT.setThreads(64);
 	}
 	
 	void iniciarV8(){
-		
+		System.out.println("TEMPERATURAS CIDADES\n");
+		configT.setThreads(80);
 	}
 	
 	void iniciarV9(){
-		
+		System.out.println("TEMPERATURAS CIDADES\n");
+		configT.setThreads(160);
 	}
 	
 	void iniciarV10(){
-		
+		System.out.println("TEMPERATURAS CIDADES\n");
+		configT.setThreads(320);
 	}
 
 }
