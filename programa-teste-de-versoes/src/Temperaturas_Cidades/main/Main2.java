9ed0e676332e38d57e73279e51aa1ea53705c634
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-09-22 13:17:01 -0300

    versão final Main2.java

--- programa-teste-de-versoes/src/Temperaturas_Cidades/main/Main2.java
+++ programa-teste-de-versoes/src/Temperaturas_Cidades/main/Main2.java
@@ -27,6 +27,7 @@ public class Main2 {
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
+		
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 11");
 		
 		//TESTE VERSÃO 12
@@ -43,142 +44,143 @@ public class Main2 {
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-	        resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 12");
-	        
-	        //TESTE VERSÃO 13
-	        for(int i = 0; i < numExecucoes; i++) {
-			long tempoInicio = System.currentTimeMillis();
-			
-			v12a20.iniciarV13();
-			
-	        long tempoFim = System.currentTimeMillis();
-
-	        tempoExecucoes[i] = tempoFim - tempoInicio;
-
-			tempoMedio +=tempoExecucoes[i];
-		}
-		tempoMedio = tempoMedio/10;
-		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
-		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 13");
 				
-		//TESTE VERSÃO 14
-       		 for(int i = 0; i < numExecucoes; i++) {
+		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 12");
+		
+		//TESTE VERSÃO 13
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
-			v12a20.iniciarV14();
-			
-	        long tempoFim = System.currentTimeMillis();
+					
+			v12a20.iniciarV13();
+					
+			long tempoFim = System.currentTimeMillis();
 
-	        tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
+				
+		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 13");
 		
+		//TESTE VERSÃO 14
+		for(int i = 0; i < numExecucoes; i++) {
+			long tempoInicio = System.currentTimeMillis();
+					
+			v12a20.iniciarV14();
+					
+			long tempoFim = System.currentTimeMillis();
+
+			tempoExecucoes[i] = tempoFim - tempoInicio;
+
+			tempoMedio +=tempoExecucoes[i];
+		}
+		tempoMedio = tempoMedio/10;
+		System.out.println("Tempo de execução médio V1: " + tempoMedio);
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 14");
 		
 		//TESTE VERSÃO 15
-        	for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV15();
-			
-	        long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-	        tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 15");
 		
 		//TESTE VERSÃO 16
-      		for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV16();
-			
-		long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-		tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 16");
-
+		
 		//TESTE VERSÃO 17
-      		for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV17();
-			
-		long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-		tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 17");
-
+		
 		//TESTE VERSÃO 18
-      		for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV18();
-			
-		long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-		tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 18");
-
+		
 		//TESTE VERSÃO 19
-      		for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV19();
-			
-		long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-		tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 19");
-
+		
 		//TESTE VERSÃO 20
-      		for(int i = 0; i < numExecucoes; i++) {
+		for(int i = 0; i < numExecucoes; i++) {
 			long tempoInicio = System.currentTimeMillis();
-			
+					
 			v12a20.iniciarV20();
-			
-		long tempoFim = System.currentTimeMillis();
+					
+			long tempoFim = System.currentTimeMillis();
 
-		tempoExecucoes[i] = tempoFim - tempoInicio;
+			tempoExecucoes[i] = tempoFim - tempoInicio;
 
 			tempoMedio +=tempoExecucoes[i];
 		}
 		tempoMedio = tempoMedio/10;
 		System.out.println("Tempo de execução médio V1: " + tempoMedio);
-		
+				
 		resultado.geraResultado(tempoExecucoes, tempoMedio, "VERSÃO 20");
 	}
 }
