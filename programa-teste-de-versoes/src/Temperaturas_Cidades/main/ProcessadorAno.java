4015758b9ca87e344c24d662bbc8f8f98be2ce59
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-09-22 13:17:58 -0300

    versão final ProcessadorAno.java

--- programa-teste-de-versoes/src/Temperaturas_Cidades/main/ProcessadorAno.java
+++ programa-teste-de-versoes/src/Temperaturas_Cidades/main/ProcessadorAno.java
@@ -9,7 +9,7 @@ import Entidades.Mes;
 
 public class ProcessadorAno extends Thread {
     String path;
-    int anoParaProcessar; 
+    int anoParaProcessar;
     Ano ano = null;
 
     ProcessadorAno(String path, int anoParaProcessar) {
@@ -20,39 +20,47 @@ public class ProcessadorAno extends Thread {
 
     public void run() {
         Mes mes = null;
-        int mesAux = 0; 
-        
+        int mesAux = 0;
+
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine(); 
             line = br.readLine(); 
+
             while (line != null) {
                 String[] vect = line.split(",");
                 int numMes = Integer.parseInt(vect[2]);
                 int numDia = Integer.parseInt(vect[3]);
                 int numAno = Integer.parseInt(vect[4]);
                 double tempMediaDiaria = Double.parseDouble(vect[5]);
+
                 if (numAno == anoParaProcessar) {
                     if (mes == null) {
                         mes = new Mes(numMes);
                         mesAux = numMes;
                     }
+
                     if (numMes != mesAux) {
                         ano.getMeses().add(mes);
                         mes = new Mes(numMes);
                         mesAux = numMes;
                     }
+
                     Dia dia = new Dia(numDia, tempMediaDiaria);
                     mes.adicionarDia(dia);
                 }
+
                 line = br.readLine();
             }
+
             if (mes != null) {
                 ano.getMeses().add(mes);
             }
+
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
     }
+
     public Ano getAno() {
         return ano;
     }
